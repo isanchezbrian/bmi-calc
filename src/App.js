59a9b9b251619fcc3f92ff4db479078722ff5a43
brians97 <isanchez.brian@gmail.com>
2023-02-01 00:17:54 -0800

    added basic css styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState('');
+  const [message, setMessage] = useState('');
+
+  let imgSrc = ''
+
+  let calcBmi = (e) => {
+    e.preventDefault()
+    
+    if(weight === 0 || height === 0) {
+      alert('Please enter a valid weight and height')
+    } else {
+      let bmi = (weight / (height * height) * 703 )
+      setBmi(bmi.toFixed(1))
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='container'>
+        <h2 className='center'>BMI Calculator</h2>
+        <form onSubmit={calcBmi}>
+          <div>
+            <label>Weight (lbs)</label>
+            <input value={weight} onChange={(e) => setWeight(e.target.value)} />
+          </div>
+          <div>
+            <label>Height (in)</label>
+            <input value={height} onChange={(e) => setHeight(e.target.value)} />
+          </div>
+          <div>
+            <button className='btn' type='submit'>Submit</button>
+            <button className='btn btn-outline'>Submit</button>
+          </div>
+        </form>
+        <div className='center'>
+          <h3>Your BMI is: {bmi}</h3>
+          <p>{message}</p>
+        </div>
+        <div className='img-container'>
+          <img src={imgSrc} alt='' />
+        </div>
+      </div>
     </div>
   );
 }
